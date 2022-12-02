0065966e9bc2946f71d370c4cc9e8e9061b39d8f
Andrew <shatokhin.a.p@gmail.com>
2022-12-02 22:43:54 +0200

    add task 2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,21 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const list = document.querySelector("#ingredients");
+
+const ingredientsList = ingredients.map(function (elem) {
+  const ingredientsItems = document.createElement("li");
+  ingredientsItems.textContent = elem;
+  ingredientsItems.classList.add("item");
+  return ingredientsItems;
+});
+console.log(ingredientsList);
+
+list.append(...ingredientsList);
+console.log(list);
