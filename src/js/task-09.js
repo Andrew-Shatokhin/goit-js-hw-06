e7fa96181c0cc384e09fe37e1b700a5374033178
Andrew <shatokhin.a.p@gmail.com>
2022-12-04 01:54:35 +0200

    add task 9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+console.log(getRandomHexColor());
+
+const bodyEl = document.querySelector("body");
+const spanEl = document.querySelector(".color");
+const changeColorButton = document.querySelector(".change-color");
+
+changeColorButton.addEventListener("click", changeBgColor);
+
+function changeBgColor() {
+  bodyEl.style.backgroundColor = `${getRandomHexColor()}`;
+  spanEl.textContent = `${getRandomHexColor()}`;
+}
